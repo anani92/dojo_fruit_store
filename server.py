be3df87f067c38b255230dffad839516a7cc6488
anani <abood.anani57@gmail.com>
2022-09-16 21:24:38 +0300

    dojo fruit initial commit

--- server.py
+++ server.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+app = Flask(__name__)
 
-@app.route('/')         
+@app.route('/', methods=['GET'])
 def index():
+    print(request.form)
+
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
+@app.route('/checkout', methods=['POST', 'GET'])
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    sb = request.form['strawberry']
+    rb = request.form['raspberry']
+    app = request.form['apple']
+    first_n = request.form['first_name']
+    last_n = request.form['last_name']
+    count = int(sb) + int(rb) + int(app)
+    print(count)
+    print(f"Charging {first_n} {last_n} for {count} fruits")
+    return render_template("checkout.html", apple=app, strawberry=sb, raspberry=rb, student_id = request.form['student_id'], first_name = first_n, last_name = last_n)
 
-@app.route('/fruits')         
+@app.route('/fruits')
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+if __name__=="__main__":
+    app.run(debug=True)
